a20ff3f24d281e8624f369832ecba0b659cd1abf
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 <otaviosantosandrigo08@gmail.com>
2024-11-23 00:49:08 -0300

    login, register arrumados :) e pagina do produto GOSTOSOASFASFGA

--- dist/js/index.js
+++ dist/js/index.js
@@ -5,8 +5,11 @@ const signup = document.querySelector(".registerscreen");
 const main = document.querySelector(".main");
 const header = document.querySelector(".header");
 const footer = document.querySelector(".footer");
-const closebtn = document.querySelector(".close_buttom")
-
+const closeLoginBtn = document.querySelector(".loginscreen .close_buttom");
+const closeSignupBtn = document.querySelector(".registerscreen .close_buttom");
+const desktop = document.querySelector(".MainDesktop");
+const entrar = document.querySelector(".register_login");
+const cadastrar = document.querySelector(".cad");
 
 
 showpass.addEventListener("click", showpassword);
@@ -37,10 +40,12 @@ function blur () {
         main.classList.add("blur");
         header.classList.add("blur");
         footer.classList.add("blur");
+        desktop.classList.add("blur");
     } else {
         main.classList.remove("blur");
         header.classList.remove("blur");
         footer.classList.remove("blur");
+        desktop.classList.remove("blur");
     }
 }
 
@@ -49,7 +54,21 @@ loginbtn.addEventListener("click", () => {
     blur();
 });
 
-closebtn.addEventListener("click", event => {
+closeLoginBtn.addEventListener("click", () => {
     close(login);
-    blur()
+    blur();
+});
+
+closeSignupBtn.addEventListener("click", () => {
+    close(signup);
+    blur();
+});
+
+entrar.addEventListener("click", () => {
+    close(signup);
+    openlogin(login);
+});
+cadastrar.addEventListener("click", () => {
+    close(login);
+    openlogin(signup);
 });
