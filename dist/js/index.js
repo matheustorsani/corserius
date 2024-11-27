cc9ea47ccd336066c6b59c16e248d59c1c1ee378
demetrius ! <matheustorsani>
2024-11-26 21:18:17 -0300

    perai

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,71 +1,62 @@
-const showpass = document.querySelector(".show_password");
-const loginbtn = document.querySelector(".login-btn");
-const login = document.querySelector(".loginscreen");
-const signup = document.querySelector(".registerscreen");
-const main = document.querySelector("main");
-const header = document.querySelector("header");
-const footer = document.querySelector("footer");
-const closeLoginBtn = document.querySelector(".loginscreen .close_buttom");
-const closeSignupBtn = document.querySelector(".registerscreen .close_buttom");
-const entrar = document.querySelector(".register_login");
-const cadastrar = document.querySelector(".cad");
+const elements = {
+    showpass: document.querySelector(".show_password"),
+    loginbtn: document.querySelector(".login-btn"),
+    login: document.querySelector(".loginscreen"),
+    signup: document.querySelector(".registerscreen"),
+    main: document.querySelector("main"),
+    header: document.querySelector("header"),
+    footer: document.querySelector("footer"),
+    closeLoginBtn: document.querySelector(".loginscreen .close_buttom"),
+    closeSignupBtn: document.querySelector(".registerscreen .close_buttom"),
+    entrar: document.querySelector(".register_login"),
+    cadastrar: document.querySelector(".cad")
+};
 
-showpass.addEventListener("click", showpassword);
+elements.showpass.addEventListener("click", tgPass);
 
-function showpassword() {
+function tgPass() {
     const pass1 = document.querySelector("#passwordreg");
     const pass2 = document.querySelector("#password2");
-    if (pass1.type === "password") {
-        pass1.type = "text";
-        pass2.type = "text";
-    } else {
-        pass1.type = "password";
-        pass2.type = "password";
-    }
+    const type = pass1.type === "password" ? "text" : "password";
+    pass1.type = type;
+    pass2.type = type;
 }
 
-function openlogin(element) {
-    element.classList.remove("hidden");
+function OpCl(element) {
+    element.classList.toggle("hidden");
 }
 
-function close(element) {
-    element.classList.add("hidden")
+function blur() {
+    elements.main.classList.toggle("blur");
+    elements.header.classList.toggle("blur");
+    elements.footer.classList.toggle("blur");
 }
 
-
-function blur () { {
-        if (!main.classList.contains("blur")) {
-            main.classList.add("blur");
-            header.classList.add("blur");
-            footer.classList.add("blur");
-        } else {
-            main.classList.remove("blur");
-            header.classList.remove("blur");
-            footer.classList.remove("blur");
-        }
-    }
-}
-
-loginbtn.addEventListener("click", () => {
-    openlogin(signup);
+elements.loginbtn.addEventListener("click", () => {
+    OpCl(elements.signup);
     blur();
 });
 
-closeLoginBtn.addEventListener("click", () => {
-    close(login);
+elements.closeLoginBtn.addEventListener("click", () => {
+    OpCl(elements.login);
     blur();
 });
 
-closeSignupBtn.addEventListener("click", () => {
-    close(signup);
+elements.closeSignupBtn.addEventListener("click", () => {
+    OpCl(elements.signup);
     blur();
 });
 
-entrar.addEventListener("click", () => {
-    close(signup);
-    openlogin(login);
+elements.entrar.addEventListener("click", () => {
+    OpCl(elements.signup);
+    OpCl(elements.login);
 });
-cadastrar.addEventListener("click", () => {
-    close(login);
-    openlogin(signup);
+
+elements.cadastrar.addEventListener("click", () => {
+    OpCl(elements.login);
+    OpCl(elements.signup);
 });
+
+
+
+
